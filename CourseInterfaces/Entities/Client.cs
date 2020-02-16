60e39a3683fe8fd719d1bc98a0801546e6474a1a
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-02-16 09:52:58 -0300

    Solução sem interfaces

---
+++ CourseInterfaces/Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public System.DateTime BirthDate { get; set; }
+    }
+}
---
+++ CourseInterfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using CourseInterfaces.Entities;
+
+namespace CourseInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (DD/MM/YYYY): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (DD/MM/YYYY): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
