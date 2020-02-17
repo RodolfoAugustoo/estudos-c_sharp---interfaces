0a4b22e5cb4cb770ff9a6abec80d19c7a72eb435
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-02-16 23:37:31 -0300

    Solução sem interface finalizada.

--- CourseInterfaces/Program.cs
+++ CourseInterfaces/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using CourseInterfaces.Entities;
+using CourseInterfaces.Services;
 
 namespace CourseInterfaces
 {
@@ -12,13 +13,26 @@ namespace CourseInterfaces
             Console.Write("Car model: ");
             string model = Console.ReadLine();
 
-            Console.Write("Pickup (DD/MM/YYYY): ");
+            Console.Write("Pickup (DD/MM/YYYY HH:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
-            Console.Write("Return (DD/MM/YYYY): ");
+            Console.Write("Return (DD/MM/YYYY HH:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            Console.Write("Price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
